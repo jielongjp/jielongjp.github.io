cc85f13e7b118f9574d2a16d203d4af6c8749b81
jielongjp <jdan000@gmail.com>
2022-10-09 15:51:47 +0900

    add dark mode

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
+//navbar scroll background
 const nav = document.querySelector(".nav");
-
 document.addEventListener("scroll", function() {
-    nav.style.backgroundColor = "#fff";
+    nav.style.backgroundColor = "var(--white)";
 })
+
+//dark mode
+function darkMode (){
+  const darkBtn = document.querySelector(".dark-mode-btn");
+    darkBtn.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+    })
+}
+darkMode ();

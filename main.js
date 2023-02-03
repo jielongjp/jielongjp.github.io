997a768d76902b78187fdb8ea3449601c9f7b20c
jielongjp <jdan000@gmail.com>
2023-02-03 22:52:57 +0900

    add simple form

--- main.js
+++ main.js
@@ -26,3 +26,32 @@ if(darkMode === "true") {
 } else {
     document.body.classList.remove("dark-mode");
 }
+
+// send form data to sheet
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwkXOXSpBpdQ4LvQiQgvQA3alZIrkUdZyQQbfgiw7ePH5WQTysXmw0DRgoDSpf8dXIC/exec'; // change with each new deployment
+const form = document.forms['sheet-form'];
+const pot = document.querySelector('[name=Subject]');
+const Message = document.querySelector('[name=Message]');
+
+form.addEventListener('submit', e => {
+  data = new FormData(form);
+  e.preventDefault();
+
+  data.append('form', form.name);
+  data.append('Message', Message.value);
+
+  if (pot.value !== '') {
+    document.querySelector('.form').innerHTML = '';
+    document.querySelector('.msg').innerHTML = '<p>Thank You. I\'ll get back to you soon</p>';
+} else {
+    fetch(scriptURL, { method: 'POST', body: data })
+        .then(response => console.log('Success: ', response))
+        .catch(error => console.error('Error: ', error.message))
+        .finally(
+            () => {
+                document.querySelector('.form').innerHTML = '';
+                document.querySelector('.msg').innerHTML = '<p>Thank You. I\'ll get back to you soon</p>';
+            }
+        )
+    }
+});

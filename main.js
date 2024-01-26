134b326bf9051165b199ab8a831ab98dfa136cb2
“jielongjp” <82949450+jielongjp@users.noreply.github.com>
2024-01-26 22:19:02 +0900

    fix console error

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-//navbar scroll background
+// navbar scroll background
 const nav = document.querySelector(".nav");
 document.addEventListener("scroll", function() {
     nav.style.backgroundColor = "var(--white)";
 })
 
-//dark mode
+// dark mode
 const darkBtn = document.querySelector(".dark-mode-btn");
 let darkMode = localStorage.getItem("darkMode")
 
-darkBtn.addEventListener("click", function darkSwitch (){
+darkBtn.addEventListener("click", function darkSwitch () {
     if (darkMode === null) {
         localStorage.setItem("darkMode", true);
         document.body.classList.toggle("dark-mode");
@@ -21,7 +21,7 @@ darkBtn.addEventListener("click", function darkSwitch (){
     }
 })
 
-if(darkMode === "true") {
+if (darkMode === "true") {
     document.body.classList.toggle("dark-mode");
 } else {
     document.body.classList.remove("dark-mode");
@@ -33,25 +33,27 @@ const form = document.forms['sheet-form'];
 const pot = document.querySelector('[name=Subject]');
 const Message = document.querySelector('[name=Message]');
 
-form.addEventListener('submit', e => {
-  data = new FormData(form);
-  e.preventDefault();
-
-  data.append('form', form.name);
-  data.append('Message', Message.value);
-
-  if (pot.value !== '') {
-    document.querySelector('.form').innerHTML = '';
-    document.querySelector('.msg').innerHTML = '<p>Thank You. I\'ll get back to you soon</p>';
-} else {
-    fetch(scriptURL, { method: 'POST', body: data })
-        .then(response => console.log('Success: ', response))
-        .catch(error => console.error('Error: ', error.message))
-        .finally(
-            () => {
-                document.querySelector('.form').innerHTML = '';
-                document.querySelector('.msg').innerHTML = '<p>Thank You. I\'ll get back to you soon</p>';
-            }
-        )
-    }
-});
+if (form) {
+    form.addEventListener('submit', e => {
+        data = new FormData(form);
+        e.preventDefault();
+    
+        data.append('form', form.name);
+        data.append('Message', Message.value);
+    
+        if (pot.value !== '') {
+        document.querySelector('.form').innerHTML = '';
+        document.querySelector('.msg').innerHTML = '<p>Thank You. I\'ll get back to you soon</p>';
+        } else {
+            fetch(scriptURL, { method: 'POST', body: data })
+                .then(response => console.log('Success: ', response))
+                .catch(error => console.error('Error: ', error.message))
+                .finally(
+                    () => {
+                        document.querySelector('.form').innerHTML = '';
+                        document.querySelector('.msg').innerHTML = '<p>Thank You. I\'ll get back to you soon</p>';
+                    }
+                )
+        }
+    });
+}
